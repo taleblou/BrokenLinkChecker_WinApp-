cf73159783f1e24e6037bb381e22f4d28b0385da
Morteza Taleblou <morteza_taleblou@yahoo.com>
2025-01-16 11:29:13 +0200

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,16 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Data;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Forms; 
-using System.ComponentModel;
+using System.Windows.Forms;
+using HtmlAgilityPack; 
+
 
 namespace BrokenLinkChecker
 {
@@ -20,6 +19,7 @@ namespace BrokenLinkChecker
         private ConcurrentBag<ErrorPage> _errorPages = new ConcurrentBag<ErrorPage>();
         private SemaphoreSlim _semaphore = new SemaphoreSlim(10); // Limit concurrent requests
         private CancellationTokenSource _cts = new CancellationTokenSource();
+        private HashSet<string> _visitedUrls = new HashSet<string>(); // Track visited URLs
         private Uri _startUri;
         private string _domain;
         private int _maxPages = 10000;
@@ -27,12 +27,12 @@ namespace BrokenLinkChecker
         public Main()
         {
             InitializeComponent();
-            dataGridViewErrors.AutoGenerateColumns = true;
-            this.FormClosing += MainForm_FormClosing;
+            dataGridViewErrors.AutoGenerateColumns = true; 
         }
 
         private void Main_Load(object sender, EventArgs e)
         {
+            progressBarCrawling.Maximum = _maxPages;
 
         }
 
@@ -68,143 +68,109 @@ namespace BrokenLinkChecker
 
             Task.Run(() => CrawlAsync(_cts.Token));
         }
+        private void ResetState()
+        {
+            _urlQueue = new ConcurrentQueue<string>();
+            _errorPages = new ConcurrentBag<ErrorPage>();
+            _visitedUrls = new HashSet<string>();
+            _visitedCount = 0;
+            _cts = new CancellationTokenSource();
+        }
+
         private async Task CrawlAsync(CancellationToken cancellationToken)
         {
             while (!_urlQueue.IsEmpty && _visitedCount < _maxPages && !cancellationToken.IsCancellationRequested)
             {
-                if (!_semaphore.Wait(100)) continue; // Wait if all slots are busy
+                if (!_semaphore.Wait(100)) continue;
 
-                string url;
-                if (!_urlQueue.TryDequeue(out url))
+                if (!_urlQueue.TryDequeue(out string url))
                 {
                     _semaphore.Release();
                     continue;
                 }
 
-                if (_visitedCount >= _maxPages)
-                {
-                    _semaphore.Release();
-                    break;
-                }
-
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    _semaphore.Release();
-                    break;
-                }
-
-                if (url.Contains(_domain))
-                {
-                    try
-                    {
-                        _visitedCount++;
-                        progressBarCrawling.Invoke((MethodInvoker)(() =>
-                        {
-                            progressBarCrawling.Value = _visitedCount;
-                        }));
-
-                        using (HttpClient client = new HttpClient())
-                        {
-                            HttpResponseMessage response = await client.GetAsync(url, cancellationToken);
-                            response.EnsureSuccessStatusCode();
-
-                            string content = await response.Content.ReadAsStringAsync();
-
-                            HtmlDocument doc = new HtmlDocument();
-                            doc.LoadHtml(content);
-
-                            // Collect all resource links
-                            var resourceLinks = new List<string>();
-                            // CSS links
-                            resourceLinks.AddRange(doc.DocumentNode.SelectNodes("//link[@rel='stylesheet']/@href")
-                                .Select(node => node.Value));
-                            // JavaScript links
-                            resourceLinks.AddRange(doc.DocumentNode.SelectNodes("//script[@src]/@src")
-                                .Select(node => node.Value));
-                            // Image links
-                            resourceLinks.AddRange(doc.DocumentNode.SelectNodes("//img[@src]/@src")
-                                .Select(node => node.Value));
-                            // Video links
-                            resourceLinks.AddRange(doc.DocumentNode.SelectNodes("//video[@src]/@src")
-                                .Select(node => node.Value));
-                            resourceLinks.AddRange(doc.DocumentNode.SelectNodes("//video/source[@src]/@src")
-                                .Select(node => node.Value));
-                            // Iframe links
-                            resourceLinks.AddRange(doc.DocumentNode.SelectNodes("//iframe[@src]/@src")
-                                .Select(node => node.Value));
-
-                            // Check each resource
-                            foreach (string link in resourceLinks)
-                            {
-                                string fullUrl = new Uri(new Uri(url), link).ToString();
-                                if (IsSameDomain(fullUrl))
-                                {
-                                    int? error = await CheckResourceAsync(fullUrl, client, cancellationToken);
-                                    if (error.HasValue && error.Value >= 400 && error.Value < 600)
-                                    {
-                                        _errorPages.Add(new ErrorPage
-                                        {
-                                            PageURL = url,
-                                            ResourceURL = fullUrl,
-                                            ErrorCode = error.Value
-                                        });
-                                        UpdateDataGridView();
-                                    }
-                                }
-                            }
-
-                            // Find all links in the page and add to queue
-                            var links = doc.DocumentNode.SelectNodes("//a[@href]")
-                                .Select(node => node.GetAttributeValue("href", ""))
-                                .Select(link => new Uri(new Uri(url), link).ToString())
-                                .Where(link => IsSameDomain(link) && !_urlQueue.Contains(link) && !_visitedUrls.Contains(link));
-
-                            foreach (string link in links)
-                            {
-                                _urlQueue.Enqueue(link);
-                            }
-                        }
-                    }
-                    catch (OperationCanceledException)
-                    {
-                        // Ignore if canceled
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show($"Error crawling {url}: {ex.Message}");
-                    }
-                    finally
-                    {
-                        _semaphore.Release();
-                    }
-                }
-                else
+                if (!_visitedUrls.Add(url))
                 {
                     _semaphore.Release();
                     continue;
+                }
+
+                try
+                {
+                    _visitedCount++;
+                    UpdateProgressBar();
+
+                    using (HttpClient client = new HttpClient())
+                    {
+                        HttpResponseMessage response = await client.GetAsync(url, cancellationToken);
+                        response.EnsureSuccessStatusCode();
+
+                        string content = await response.Content.ReadAsStringAsync();
+                        var doc = new HtmlAgilityPack.HtmlDocument();
+                        doc.LoadHtml(content);
+
+                        await ProcessResourcesAsync(url, doc, client, cancellationToken);
+                        EnqueueLinks(url, doc);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error crawling {url}: {ex.Message}");
+                }
+                finally
+                {
+                    _semaphore.Release();
                 }
             }
 
-            // Save error details to CSV
             SaveErrorsToCSV();
-
-            Invoke((MethodInvoker)(() =>
-            {
-                //lblStatus.Text = "Crawling completed.";
-                Check_Button.Enabled = true;
-            }));
+            Invoke((MethodInvoker)(() => Check_Button.Enabled = true));
         }
 
-        private bool IsSameDomain(string url)
+        private async Task ProcessResourcesAsync(string url, HtmlAgilityPack.HtmlDocument doc, HttpClient client, CancellationToken cancellationToken)
         {
-            try
+            var resourceLinks = doc.DocumentNode.SelectNodes("//link[@rel='stylesheet' or @src]")
+                ?.Select(node => node.GetAttributeValue("href", node.GetAttributeValue("src", null)))
+                .Where(link => !string.IsNullOrEmpty(link))
+                .Distinct();
+
+            if (resourceLinks != null)
             {
-                Uri uri = new Uri(url);
-                return uri.Host == _domain;
+                foreach (string link in resourceLinks)
+                {
+                    string fullUrl = new Uri(new Uri(url), link).ToString();
+                    if (IsSameDomain(fullUrl))
+                    {
+                        int? errorCode = await CheckResourceAsync(fullUrl, client, cancellationToken);
+                        if (errorCode.HasValue && errorCode.Value >= 400)
+                        {
+                            _errorPages.Add(new ErrorPage
+                            {
+                                PageURL = url,
+                                ResourceURL = fullUrl,
+                                ErrorCode = errorCode.Value
+                            });
+                            UpdateDataGridView();
+                        }
+                    }
+                }
             }
-            catch (UriFormatException)
+        }
+
+        private void EnqueueLinks(string url, HtmlAgilityPack.HtmlDocument doc)
+        {
+            var links = doc.DocumentNode.SelectNodes("//a[@href]")
+                ?.Select(node => node.GetAttributeValue("href", ""))
+                .Where(link => !string.IsNullOrEmpty(link))
+                .Select(link => new Uri(new Uri(url), link).ToString())
+                .Where(link => IsSameDomain(link) && !_visitedUrls.Contains(link));
+
+            if (links != null)
             {
-                return false;
+                foreach (string link in links)
+                {
+                    _urlQueue.Enqueue(link);
+                }
             }
         }
 
@@ -213,12 +179,14 @@ namespace BrokenLinkChecker
             try
             {
                 HttpResponseMessage response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, url), cancellationToken);
-                response.EnsureSuccessStatusCode();
+                response.EnsureSuccessStatusCode();  // Throws an exception if the status code is not successful
                 return (int?)response.StatusCode;
             }
             catch (HttpRequestException ex)
             {
-                return (int?)ex.StatusCode;
+                // Handle HttpRequestException (e.g., no network connection, invalid URL, etc.)
+                // In this case, we can return the status code from the exception message
+                return null; // You can customize this as needed (e.g., logging or returning a specific error code)
             }
             catch (TaskCanceledException)
             {
@@ -230,30 +198,49 @@ namespace BrokenLinkChecker
             }
         }
 
+        private void UpdateProgressBar()
+        {
+            progressBarCrawling.Invoke((MethodInvoker)(() => progressBarCrawling.Value = Math.Min(_visitedCount, _maxPages)));
+        }
+
         private void UpdateDataGridView()
         {
             if (dataGridViewErrors.InvokeRequired)
             {
-                dataGridViewErrors.Invoke((MethodInvoker)(() => UpdateDataGridView()));
+                dataGridViewErrors.Invoke((MethodInvoker)UpdateDataGridView);
                 return;
             }
 
-            List<ErrorPage> errors = _errorPages.ToList();
-            dataGridViewErrors.DataSource = errors;
+            dataGridViewErrors.DataSource = _errorPages.ToList();
         }
 
         private void SaveErrorsToCSV()
         {
-            List<ErrorPage> errors = _errorPages.ToList();
-            if (errors.Any())
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "error_details.csv");
+
+            using (StreamWriter writer = new StreamWriter(filePath, false, Encoding.UTF8))
             {
-                DataFrame df = DataFrame.FromRecords(errors);
-                df.ToCSV("error_details.csv");
+                writer.WriteLine("PageURL,ResourceURL,ErrorCode");
+                foreach (var error in _errorPages)
+                {
+                    writer.WriteLine($"{error.PageURL},{error.ResourceURL},{error.ErrorCode}");
+                }
             }
-            else
+
+            MessageBox.Show($"Error details saved to {filePath}");
+        }
+
+        private bool IsSameDomain(string url)
+        {
+            try
             {
-                MessageBox.Show("No error pages to save.");
+                return new Uri(url).Host == _domain;
+            }
+            catch
+            {
+                return false;
             }
         }
     }
+ 
 }
